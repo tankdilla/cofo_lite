4aa6addbe4dd95f6f459965d5a1da0925ba2a7c2
Darrell Johnson <darrellj@gmail.com>
2013-12-16 21:31:30 -0600

    added pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613023011) do
+ActiveRecord::Schema.define(:version => 20131217024339) do
 
   create_table "chord_definitions", :force => true do |t|
     t.integer  "chord_id"
@@ -131,6 +131,24 @@ ActiveRecord::Schema.define(:version => 20130613023011) do
     t.string   "time_signature"
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
   create_table "verses", :force => true do |t|
     t.integer  "song_id"
     t.integer  "line_number"
@@ -138,6 +156,13 @@ ActiveRecord::Schema.define(:version => 20130613023011) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "song_name_id"
+  end
+
+  create_table "voicings", :force => true do |t|
+    t.integer  "chord_number"
+    t.string   "chord_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "word_notes", :force => true do |t|
