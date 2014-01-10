4a21621bcecf86a7048526ddd29b7c5f055dd725
Darrell Johnson <darrellj@gmail.com>
2014-01-10 11:12:03 -0600

    play a single chord, tweaked delay

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -2,29 +2,37 @@ $(document).on('ready page:load', function(){
 
   MIDI.loadPlugin({
     soundfontUrl: "../../assets/soundfont/",
-    instrument: "acoustic_grand_piano",
-    callback: function() { }
+    instrument: "acoustic_grand_piano"
   });
   
-  $('a#playnote').on('click', function(){
+  $('a#play_song').on('click', function(){
     position_notes = $('#all-notes').data('notes')
     
     $(position_notes).each(function(i){
 
-      var channel = 0,
-          chord = $(this).map(function(note){
-            return MIDI.keyToNote[this]
-          }),
-          velocity = 127,
-          delay = 1;
-      
-      MIDI.setVolume(0, 127);
-      MIDI.chordOn(channel, chord, velocity, delay);
-      MIDI.chordOff(channel, chord, delay + 0.75);
-      $(this).delay(1000*i)
+      playChord(this, 0.75*i)
+
     });
       
   });
 
+  $('a#play_chord').on('click', function(){
+    notes = $(this).data('notes')
+    
+    playChord(notes, 0)
+
+  });
+  
 });
-4
+
+function playChord(chord, delay){
+  var channel = 0,
+      chord = $(chord).map(function(note){
+        return MIDI.keyToNote[this]
+      }),
+      velocity = 127;
+  
+  MIDI.setVolume(0, 127);
+  MIDI.chordOn(channel, chord, velocity, delay);
+  MIDI.chordOff(channel, chord, delay + 0.75);
+}
