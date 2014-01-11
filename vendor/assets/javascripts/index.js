ed909bd1e3c0292fd757a468df4c2805dc945e00
Darrell Johnson <darrellj@gmail.com>
2014-01-10 18:37:00 -0600

    moving files around

---
+++ vendor/assets/javascripts/index.js
@@ -0,0 +1,8 @@
+//= require ./js/MIDI/AudioDetect
+//= require ./js/MIDI/LoadPlugin
+//= require ./js/MIDI/Player
+//= require ./js/MIDI/Plugin
+//= require ./js/Window/DOMLoader.XMLHttp
+//= require ./js/Window/DOMLoader.script
+//= require ./inc/Base64
+//= require ./inc/base64binary
