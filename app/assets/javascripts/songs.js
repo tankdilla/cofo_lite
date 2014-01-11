bccf6beca126bdc23b649c741152ebb195675354
Darrell Johnson <darrellj@gmail.com>
2014-01-10 18:26:34 -0600

    ...

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -4,25 +4,20 @@ $(document).on('ready page:load', function(){
     soundfontUrl: "../../assets/soundfont/",
     instrument: "acoustic_grand_piano"
   });
-  
+
   $('a#play_song').on('click', function(){
     position_notes = $('#all-notes').data('notes')
-    
+
     $(position_notes).each(function(i){
-
       playChord(this, 0.75*i)
-
     });
-      
   });
 
   $('a#play_chord').on('click', function(){
     notes = $(this).data('notes')
-    
     playChord(notes, 0)
-
   });
-  
+
 });
 
 function playChord(chord, delay){
@@ -31,7 +26,7 @@ function playChord(chord, delay){
         return MIDI.keyToNote[this]
       }),
       velocity = 127;
-  
+
   MIDI.setVolume(0, 127);
   MIDI.chordOn(channel, chord, velocity, delay);
   MIDI.chordOff(channel, chord, delay + 0.75);
